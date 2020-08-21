b6f861fd3331b226a2f93a53d977c2f850868649
akalya6601 <69978250+akalya6601@users.noreply.github.com>
2020-08-21 08:38:29 -0700

    Add files via upload

---
+++ social_distancing_configuration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 20:04:10 2020
+
+@author: ARUNAKAL
+"""
+
+MODEL_PATH = "yolo-coco"
+
+MIN_CONF= 0.3
+
+NMS_THRESH = 0.3
+
+USE_GPU = False
+
+MIN_DISTANCE = 50
